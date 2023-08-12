4dcc5b909cb385fe10e924b489d4d72ef8f47eef
Mohamed-Sheriif <mohamedsh2782@gmail.com>
2023-08-12 16:59:16 +0300

    design category schema

---
+++ models/categoryModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+// 1. Create a schema
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required!"],
+      unique: [true, "Category name must be unique!"],
+      minlength: [3, "Too short category name!"],
+      maxlength: [32, "Too long category name!"],
+    },
+    // Car and Phones => shopping/car-and-phones
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+  },
+  { timeseries: true }
+);
+
+// 2. Create a model
+const CategoryModel = mongoose.model("Category", categorySchema);
+
+// 3. Export the model
+module.exports = CategoryModel;

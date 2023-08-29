eaf7a060ad4ff7c7094535226f258e277d44122e
Mohamed-Sheriif <mohamedsh2782@gmail.com>
2023-08-29 15:20:14 +0300

    add user model

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please enter your name!"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email!"],
+      unique: true,
+      lowercase: true,
+    },
+    phone: String,
+    profileImage: String,
+    password: {
+      type: String,
+      required: [true, "Please enter your password!"],
+      minlength: [6, "Your password must be longer than 6 characters!"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
